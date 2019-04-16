bc2da3f5f68817756004304ae323a297ae38d5b2
Brian Hensley and Lindsey Baker <brian+lindsey@>
2019-04-16 11:05:26 -0700

    Add hide to click function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 $(function() {
   $(".paper").click(function(event) {
     event.preventDefault();
+      $("figure").hide();
     $(".paperGrid").toggle();
   });
   $(".plastic").click(function(event) {
     event.preventDefault();
+      $("figure").hide();
     $(".plasticGrid").toggle();
   });
   $(".metal").click(function(event) {
     event.preventDefault();
+      $("figure").hide();
     $(".metalGrid").toggle();
   });
   $(".glass").click(function(event) {
     event.preventDefault();
+      $("figure").hide();
     $(".glassGrid").toggle();
   });
   $(".trash").click(function(event) {
     event.preventDefault();
+      $("figure").hide();
     $(".trashGrid").toggle();
   });
   $(".noCurb").click(function(event) {
     event.preventDefault();
+    $("figure").hide();
     $(".noCurbGrid").toggle();
   });
 });
