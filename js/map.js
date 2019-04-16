948e2520146d536356c68981f080f9db83280647
Student <example@example.com>
2019-04-16 15:45:07 -0700

    add a listner on the marker so on click it willl show the name of the company

--- js/map.js
+++ js/map.js
@@ -46,6 +46,10 @@ function initMap() {
         var infoWindow = new google.maps.InfoWindow({
           content:props.content
         })
+
+        marker.addListener('click', function(){
+          infoWindow.open(map, marker);
+        });
       }
     }
   }
