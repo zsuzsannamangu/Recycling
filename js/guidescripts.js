76c3df88c6165eaea4ab5919186e90f272b29a8d
Student <example@example.com>
2019-04-17 12:01:49 -0700

    add function to guide.js to look for a class linked to search options which identify a section of the guide.html

--- js/guidescripts.js
+++ js/guidescripts.js
@@ -30,3 +30,32 @@ $(function() {
     $(".noCurbGrid").toggle();
   });
 });
+
+$(function() {
+  $("#myList").click(function() {
+    if ( element.classList.contains("paper-value")) {
+      $("figure, .text").hide();
+      $(".paperGrid").toggle();
+    }
+    if (element.classList.contains("plastic-value")) {
+      $("figure, .text").hide();
+      $(".plasticGrid, .plastic-text").toggle();
+    }
+    if ( === "metal-value") {
+      $("figure, .text").hide();
+      $(".metalGrid").toggle();
+    }
+    if ( === "glass-value") {
+      $("figure, .text").hide();
+      $(".glassGrid, .glass-text").toggle();
+    }
+    if ( === "trash-value") {
+      $("figure, .text").hide();
+      $(".trashGrid, .trash-text").toggle();
+    }
+    if ( === "noCurb-value") {
+      $("figure, .text").hide();
+      $(".noCurbGrid").toggle();
+    }
+  })
+});
