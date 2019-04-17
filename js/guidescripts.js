ead9d15ff0b7d6de7ce77ef8989164bb07d6e488
Student <example@example.com>
2019-04-17 16:22:47 -0700

    it will now which option is selected in the input list but nothing else

--- js/guidescripts.js
+++ js/guidescripts.js
@@ -31,31 +31,78 @@ $(function() {
   });
 });
 
-$(function() {
-  $("#myList").click(function() {
-    if ( element.classList.contains("paper-value")) {
+// function myList(){
+//   if ($("option").hasClass('paper-value') === true) {
+//     alert("paper")
+//     $("figure, .text").hide();
+//     $(".paperGrid").toggle();
+//   } else if ($('option').hasClass('plastic-value') === true) {
+//     alert("plastic")
+//     $("figure, .text").hide();
+//     $(".plasticGrid, .plastic-text").toggle();
+//   } else if ($('option').hasClass('metal-value') === true) {
+//     alert("metal")
+//     $("figure, .text").hide();
+//     $(".metalGrid").toggle();
+//   } else if ($('option').hasClass('glass-value') === true) {
+//     alert("glass")
+//     $("figure, .text").hide();
+//     $(".glassGrid, .glass-text").toggle();
+//   } else if ($('option').hasClass('trash-value') === true) {
+//     alert("trash")
+//     $("figure, .text").hide();
+//     $(".trashGrid, .trash-text").toggle();
+//   } else if($('option').hasClass('noCurb-value') === true) {
+//     alert("special")
+//     $("figure, .text").hide();
+//     $(".noCurbGrid").toggle();
+//   }
+// }
+
+
+$(function(){
+
+  $('#searchForm').submit(function(event) {
+    event.preventDefault();
+    var userSearchInput = $("#searchList").val()
+    userSearchInput
+    var optionSearch = document.getElementsByTagName("option");
+    console.log(typeof optionSearch);
+    for (var i = 0; i < = optionSearch.length; i++) {
+      if (userSearchInput === optionSearch.val()) {
+        alert(optionSearch.val());
+      }
+    }
+    console.log(userSearchInput);
+    console.log(optionSearch);
+
+    // var to store user input
+    // compare input to values in #myList -> google method
+    // on a match, show that value's associated class
+    if (userSearchInput) {
+      // alert("paper")
       $("figure, .text").hide();
       $(".paperGrid").toggle();
-    }
-    if (element.classList.contains("plastic-value")) {
+    } else if ($('option').hasClass('plastic-value')) {
+      // alert("plastic")
       $("figure, .text").hide();
       $(".plasticGrid, .plastic-text").toggle();
-    }
-    if ( === "metal-value") {
+    } else if ($('option').hasClass('metal-value')) {
+      // alert("metal")
       $("figure, .text").hide();
       $(".metalGrid").toggle();
-    }
-    if ( === "glass-value") {
+    } else if ($('option').hasClass('glass-value')) {
+      // alert("glass")
       $("figure, .text").hide();
       $(".glassGrid, .glass-text").toggle();
-    }
-    if ( === "trash-value") {
+    } else if ($('option').hasClass('trash-value')) {
+      // alert("trash")
       $("figure, .text").hide();
       $(".trashGrid, .trash-text").toggle();
-    }
-    if ( === "noCurb-value") {
+    } else if($('option').hasClass('noCurb-value')) {
+      // alert("special")
       $("figure, .text").hide();
       $(".noCurbGrid").toggle();
-    }
-  })
+    } else {};
+  });
 });
