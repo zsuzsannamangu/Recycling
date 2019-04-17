a922ef5727e05c4397c8cd3d7df61186266bf5db
Student <example@example.com>
2019-04-17 11:13:13 -0700

    Add links to .footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
-// $(function(){
-//   $(".quiz").click(function(event) {
-//     event.preventDefault();
-//     alert("quiz");
-//   });
-//   $(".guide").click(function(event) {
-//     event.preventDefault();
-//     alert("guide");
-//   });
-//   $(".map").click(function(event) {
-//     event.preventDefault();
-//     alert("map");
-//   });
-// });
+
+$(function(){
+  var modal = document.getElementById("pickUpLocator");
+  var span = document.getElementsByClassName("close")[0];
+  $("li#pickup").click(function(event) {
+    event.preventDefault();
+    modal.style.display = "block";
+  });
+  span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+});
